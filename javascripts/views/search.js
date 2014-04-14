1346f2a81cd11ba5f26bcb0afe504a28cffd9687
Edwin van der Graaf <edwin@digitpaint.nl>
2014-04-14 12:56:46 +0200

    Displaying search results

--- javascripts/views/search.js
+++ javascripts/views/search.js
@@ -5,6 +5,9 @@
     
     initialize: function(){
       this.results = new App.SearchCollection();
+
+      // Collection event
+      this.results.on("sync", this.renderResults, this);
     },
 
     events: {
@@ -25,6 +28,34 @@
 
     search: function(query){
       this.results.fetch({data: query});
+    },
+
+    renderResults: function(collection){
+      var resultsTable = this.$el.find("table tbody");
+
+      // Clean old results first
+      resultsTable.html("");
+
+      collection.each(function(result){
+        var resultView = new App.SearchResultView({model: result}).render();
+        resultsTable.append(resultView.$el);
+      });
+    }
+  });
+
+  App.SearchResultView = Backbone.View.extend({
+    template: _.template($("#search-result-view").html()),
+    tagName: "tr",
+    events: {
+      "click .play" : "play"
+    },
+    render: function(){
+      this.$el.html(this.template(this.model.attributes));
+      return this;
+    },
+    play: function(){
+      alert("Add to playlist and play!");
+      return event.preventDefault();
     }
   });
 
